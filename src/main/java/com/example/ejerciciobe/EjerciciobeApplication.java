9af04b43678a1f117967273ee289be5f7f8c5d7a
Matias Greco <matiasdgreco@gmail.com>
2020-01-30 09:51:50 -0300

    Initial commit

---
+++ src/main/java/com/example/ejerciciobe/EjerciciobeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciobeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejerciciobe/EjerciciobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
