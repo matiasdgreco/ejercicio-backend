59593f4579b9c70c5d9e7e2809ad3845b6142be3
Matias Greco <matiasdgreco@gmail.com>
2020-01-31 16:28:32 -0300

    Add CartRequest class

---
+++ src/main/java/com/example/ejerciciobe/Entity/CartRequest.java
@@ -0,0 +1,14 @@
+package com.example.ejerciciobe.Entity;
+
+public class CartRequest {
+    private String fullName;
+    private String email;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
